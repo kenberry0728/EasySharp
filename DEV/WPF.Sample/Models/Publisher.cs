88d7a3fae23f9b4f7adc7ddc228f258b673373fb
Kenjiro Nagao <toto1010@hotmail.com>
2019-05-07 16:31:01 +0900

    ToStringForPublisher

--- DEV/WPF.Sample/Models/Publisher.cs
+++ DEV/WPF.Sample/Models/Publisher.cs
@@ -1,14 +1,26 @@
-﻿namespace EasySharpWpf.Sample.Models
+﻿using System.Linq;
+
+using EasySharpStandard.Attributes.Core;
+using EasySharpStandard.Reflections.Core;
+using EasySharpWpf.ViewModels.Rails.Attributes;
+
+namespace EasySharpWpf.Sample.Models
 {
     public class Publisher
     {
+        [RailsBind]
         public string Name { get; set; }
 
+        [RailsBind]
         public PublisherType PublisherType { get; set; }
 
         public override string ToString()
         {
-            return this.Name;
+            return string.Join(
+                    ", ", 
+                    this.GetType().GetProperties()
+                    .Where(p => p.HasCustomAttribute<RailsBindAttribute>())
+                    .Select(p => $"{p.GetDisplayName()} : {p.GetValue(this)}"));
         }
     }
 }
