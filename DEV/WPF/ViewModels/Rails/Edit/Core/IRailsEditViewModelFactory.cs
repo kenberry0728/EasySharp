4a7c70c21c8805ebb362850074b02436d4b540c5
Kenjiro Nagao <toto1010@hotmail.com>
2019-05-21 08:27:04 +0900

    interface

--- DEV/WPF/ViewModels/Rails/Edit/Core/IRailsEditViewModelFactory.cs
+++ DEV/WPF/ViewModels/Rails/Edit/Core/IRailsEditViewModelFactory.cs
@@ -1,9 +1,10 @@
 ﻿using EasySharpWpf.ViewModels.Rails.Core.Edit;
+using System;
 
 namespace EasySharpWpf.ViewModels.Rails.Edit.Core
 {
     public interface IRailsEditViewModelFactory
     {
-        IRailsEditViewModel Create();
+        IRailsEditViewModel Create(object model, Type type = null);
     }
 }
---
+++ DEV/WPF/ViewModels/Rails/Edit/Implementation/RailsEditViewModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using EasySharpWpf.ViewModels.Rails.Core.Edit;
+using EasySharpWpf.ViewModels.Rails.Edit.Core;
+using System;
+
+namespace EasySharpWpf.ViewModels.Rails.Edit.Implementation
+{
+    internal class RailsEditViewModelFactory : IRailsEditViewModelFactory
+    {
+        public IRailsEditViewModel Create(object model, Type type = null)
+        {
+            return new RailsEditViewModel(model, type);
+        }
+    }
+}
