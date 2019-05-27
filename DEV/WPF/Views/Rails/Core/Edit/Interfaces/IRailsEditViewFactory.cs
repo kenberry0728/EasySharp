7ababe2521ace08f134af0de64abf77f21f062d0
Kenjiro Nagao <toto1010@hotmail.com>
2019-05-27 17:10:44 +0900

    use using.

--- DEV/WPF/Views/Rails/Core/Edit/Interfaces/IRailsEditViewFactory.cs
+++ DEV/WPF/Views/Rails/Core/Edit/Interfaces/IRailsEditViewFactory.cs
@@ -1,4 +1,5 @@
 ﻿using EasySharpWpf.ViewModels.Rails.Core.Edit;
+using EasySharpWpf.ViewModels.Rails.Edit.Core;
 using System;
 using System.Windows;
 
@@ -6,8 +7,7 @@ namespace EasySharpWpf.Views.Rails.Core.Edit
 {
     public interface IRailsEditViewFactory
     {
-        ViewModels.Rails.Edit.Core.IRailsBindCreator RailsBindCreator { get; }
-
+        IRailsBindCreator RailsBindCreator { get; }
         FrameworkElement CreateEditView(object model, Type type = null);
         bool? ShowEditWindow(object initialValueModel, Type type, out object editedModel);
         bool? ShowEditWindow(Type type, out object editedModel);
