5b0b2950cd7e6a0718c01b1e9e4ce2e7270945bb
Kenjiro Nagao <toto1010@hotmail.com>
2019-05-27 16:32:00 +0900

    Adding Xamarin Forms default template.

---
+++ DEV/EasySharpXamarinForms/EasySharpXamarinForms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using EasySharpXamarinForms.Models;
+
+namespace EasySharpXamarinForms.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
