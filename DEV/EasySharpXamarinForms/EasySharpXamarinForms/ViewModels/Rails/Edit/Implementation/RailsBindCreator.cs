214fa7c4d9997baede8144b8810a9fc08832ed56
Kenjiro Nagao <toto1010@hotmail.com>
2019-05-28 23:02:56 +0900

    Adding RainlsBindCreator

---
+++ DEV/EasySharpXamarinForms/EasySharpXamarinForms/ViewModels/Rails/Edit/Implementation/RailsBindCreator.cs
@@ -0,0 +1,36 @@
+﻿using EasySharpStandardMvvm.ViewModels.Core;
+using EasySharpXamarinForms.ViewModels.Rails.Edit.Core;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace EasySharpXamarinForms.ViewModels.Rails.Edit.Implementation
+{
+    internal class RailsBindCreator : ViewModelBase, IRailsBindCreator
+    {
+        public string GetRailsProperyPath(PropertyInfo propertyInfo)
+        {
+            return $"[{this.GetPropertyName(propertyInfo)}]";
+        }
+
+        public string GetRailsPropertyName(string bindingPath)
+        {
+            return bindingPath.Trim(new[] { '[', ']' });
+        }
+
+        public Binding CreateRailsBinding(PropertyInfo propertyInfo)
+        {
+            var bindingPath = this.GetRailsProperyPath(propertyInfo);
+            var binding = new Binding(bindingPath)
+            {
+                Mode = BindingMode.TwoWay,
+            };
+
+            return binding;
+        }
+
+        public string GetPropertyName(PropertyInfo propertyInfo)
+        {
+            return propertyInfo.Name;
+        }
+    }
+}
