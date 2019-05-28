c96206248ef10134a2813a504652d938c95edf70
Kenjiro Nagao <toto1010@hotmail.com>
2019-05-29 06:39:14 +0900

    Improve DelegateCommand

--- DEV/WPF/Commands/Core/DelegateCommand.cs
+++ DEV/WPF/Commands/Core/DelegateCommand.cs
@@ -3,17 +3,14 @@ using System.Windows.Input;
 
 namespace EasySharpWpf.Commands.Core
 {
-    public class DelegateCommand : ICommand
+    public class DelegateCommand : CommandBase, ICommand
     {
-        private readonly Func<object, bool> canExecute;
-        private readonly Action<object> execute;
-
-        public event EventHandler CanExecuteChanged;
+        private readonly Action<object> executeFunc;
 
         public DelegateCommand(Func<object, bool> canExecute, Action<object> execute)
+            :base(canExecute)
         {
-            this.canExecute = canExecute;
-            this.execute = execute;
+            this.executeFunc = execute;
         }
 
         public DelegateCommand(Action<object> execute)
@@ -23,17 +20,12 @@ namespace EasySharpWpf.Commands.Core
 
         public bool CanExecute(object parameter)
         {
-            return this.canExecute(parameter);
+            return this.CanExecuteFunc(parameter);
         }
 
         public void Execute(object parameter)
         {
-            this.execute(parameter);
-        }
-
-        public void NotifyCanExecuteChanged(EventArgs e)
-        {
-            this.CanExecuteChanged?.Invoke(this, e);
+            this.executeFunc(parameter);
         }
     }
 }
