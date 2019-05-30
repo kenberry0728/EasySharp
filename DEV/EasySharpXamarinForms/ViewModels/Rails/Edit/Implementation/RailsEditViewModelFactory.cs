82bc7528b77837d9749e7049709af79f6c53fd4f
Kenjiro Nagao <toto1010@hotmail.com>
2019-05-31 06:33:12 +0900

    move

--- DEV/EasySharpXamarinForms/ViewModels/Rails/Edit/Implementation/RailsEditViewModelFactory.cs
+++ DEV/EasySharpXamarinForms/ViewModels/Rails/Edit/Implementation/RailsEditViewModelFactory.cs
@@ -3,7 +3,6 @@ using EasySharpWpf.ViewModels.Rails.Core.Edit;
 using EasySharpWpf.ViewModels.Rails.Edit.Core;
 using EasySharpXamarinForms.Sample.ViewModels.Rails.Edit.Implementation;
 using System;
-using System.Windows.Data;
 using Xamarin.Forms;
 
 namespace EasySharpWpf.ViewModels.Rails.Edit.Implementation
