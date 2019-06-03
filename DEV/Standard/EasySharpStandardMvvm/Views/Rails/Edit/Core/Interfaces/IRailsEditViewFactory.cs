1aac76ccba9302d27c6252f35b0c48748ff81815
Kenjiro Nagao <toto1010@hotmail.com>
2019-06-03 16:58:31 +0900

    rename

--- DEV/Standard/EasySharpStandardMvvm/Views/Rails/Edit/Core/Interfaces/IRailsEditViewFactory.cs
+++ DEV/Standard/EasySharpStandardMvvm/Views/Rails/Edit/Core/Interfaces/IRailsEditViewFactory.cs
@@ -4,10 +4,10 @@ using System;
 
 namespace EasySharpWpf.Views.Rails.Core.Edit
 {
-    public interface IRailsEditViewFactory<TBinding, TUIBase>
+    public interface IRailsEditViewFactory<TBinding, TViewControl>
     {
         IRailsBindCreator<TBinding> RailsBindCreator { get; }
-        TUIBase CreateEditView(object model, Type type = null);
+        TViewControl CreateEditView(object model, Type type = null);
         bool? ShowEditWindow(object initialValueModel, Type type, out object editedModel);
         bool? ShowEditWindow(Type type, out object editedModel);
         void Edit(IRailsEditViewModel viewModel);
