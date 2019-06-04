7e7b0ceaafbe0ca051022dda658526da0e169214
Kenjiro Nagao <toto1010@hotmail.com>
2019-06-04 23:36:16 +0900

    Adding RailsCandidatesStringAttribute

---
+++ DEV/Standard/EasySharpStandardMvvm/Attributes/Rails/RailsCandidatesStringAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EasySharpStandardMvvm.Rails.Attributes
+{
+    [AttributeUsage(
+        AttributeTargets.Property,
+        AllowMultiple = false,
+        Inherited = false)]
+    public class RailsCandidatesStringAttribute : RailsBindAttribute
+    {
+        #region Constructors
+
+        public RailsCandidatesStringAttribute()
+        : this(true)
+        {
+        }
+
+        public RailsCandidatesStringAttribute(bool userVisible)
+            : base(userVisible)
+        {
+        }
+
+        public RailsCandidatesStringAttribute(string elementName)
+            : base(elementName)
+        {
+            this.ElementName = elementName;
+        }
+
+        #endregion
+
+        public string CandidatesFileName { get; set; }
+
+    }
+}
