45c3709c2f6d9fc84169fb26f49c009913cfdc8b
Kenjiro Nagao <toto1010@hotmail.com>
2019-06-10 05:53:20 +0900

    Refactoring

--- DEV/WPF/ViewModels/Rails/Index/Implementation/RailsIndexViewModel.cs
+++ DEV/WPF/ViewModels/Rails/Index/Implementation/RailsIndexViewModel.cs
@@ -1,28 +1,24 @@
-﻿using EasySharpStandard.Collections.Core;
+﻿using System;
+using System.Collections;
+using System.Linq;
+using EasySharpStandard.Collections.Core;
 using EasySharpStandardMvvm.ViewModels.Core;
 using EasySharpStandardMvvm.ViewModels.Rails.Edit.Core;
 using EasySharpStandardMvvm.ViewModels.Rails.Index.Core;
 using EasySharpWpf.ViewModels.Rails.Edit.Core;
-using System;
-using System.Collections;
-using System.Linq;
-using System.Windows.Data;
 
-namespace EasySharpWpf.ViewModels.Rails.Implementation.Index
+namespace EasySharpWpf.ViewModels.Rails.Index.Implementation
 {
-    internal class RailsIndexViewModel : RailsIndexViewModelBase, IRailsIndexViewModel
+    internal class RailsIndexViewModel : RailsIndexViewModelBase
     {
-        private readonly IRailsEditViewModelFactory<Binding> railsEditViewModelFactory;
-
         public RailsIndexViewModel(
             IList modelList,
             Type type,
             IRailsEditViewModelFactory railsEditViewModelFactory = null)
         {
-            this.railsEditViewModelFactory = railsEditViewModelFactory.Resolve();
-
+            railsEditViewModelFactory = railsEditViewModelFactory.Resolve();
             this.ItemsSource = new ObservableModelLinkedCollection<IRailsEditViewModel>(
-                modelList.ToEnumerable().Select(m => this.railsEditViewModelFactory.Create(m)),
+                modelList.ToEnumerable().Select(m => railsEditViewModelFactory.Create(m)),
                 modelList);
             this.ItemType = type;
         }
--- DEV/WPF/Views/Rails/Core/Index/DefaultRailsIndexViewFactory.cs
+++ DEV/WPF/Views/Rails/Core/Index/DefaultRailsIndexViewFactory.cs
@@ -3,7 +3,6 @@ using EasySharpStandardMvvm.Attributes.Rails;
 using EasySharpStandardMvvm.Commands.Core;
 using EasySharpStandardMvvm.Views.Rails.Core;
 using EasySharpWpf.ViewModels.Rails.Edit.Implementation;
-using EasySharpWpf.ViewModels.Rails.Implementation.Index;
 using EasySharpWpf.Views.EasyViews.Core;
 using EasySharpWpf.Views.Layouts.Core;
 using EasySharpWpf.Views.Rails.Core.Edit;
@@ -18,6 +17,7 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Input;
+using EasySharpWpf.ViewModels.Rails.Index.Implementation;
 
 namespace EasySharpWpf.Views.Rails.Core.Index
 {
