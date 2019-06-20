7566caaf09c348d1e846a04b074b5096444c4b9f
Kenjiro Nagao <toto1010@hotmail.com>
2019-06-20 23:12:17 +0900

    CheckUpdate

---
+++ DEV/Applications/NetFramework/Console/AppInstaller/CheckUpdate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AppInstaller.Core.Arguments;
+using AppInstaller.Core.Results;
+
+namespace AppInstaller
+{
+    public class CheckUpdate
+    {
+        public AppInstallerResult Run(AppInstallerArgument appInstallerArgument)
+        {
+            var excludeRegexList = appInstallerArgument.ExcludePathRegex.Select(ex => new Regex(ex)).ToList();
+
+            var sourceDirInfo = new DirectoryInfo(appInstallerArgument.SourceDir);
+            var sourceLastUpdateDate = GetLastWriteTimeUtc(sourceDirInfo, excludeRegexList);
+
+            var installDirInfo = new DirectoryInfo(appInstallerArgument.InstallDir);
+            var installLastUpdateDate = GetLastWriteTimeUtc(installDirInfo, excludeRegexList);
+            return new AppInstallerResult
+            {
+                ResultCode = ResultCode.Success,
+                Updated = sourceLastUpdateDate > installLastUpdateDate
+            };
+        }
+
+        private static DateTime GetLastWriteTimeUtc(DirectoryInfo targetDirectoryInfo, IEnumerable<Regex> regex)
+        {
+            return targetDirectoryInfo.GetFiles("*", SearchOption.AllDirectories)
+                .Where(f => f.IsTargetFile(targetDirectoryInfo.FullName, regex))
+                .Max(f => f.LastWriteTimeUtc);
+        }
+
+    }
+}
--- DEV/Applications/NetFramework/Console/AppInstaller/Program.cs
+++ DEV/Applications/NetFramework/Console/AppInstaller/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using AppInstaller.Core.Arguments;
 using AppInstaller.Core.Results;
 using AppInstaller.Implementation;
@@ -56,7 +54,7 @@ namespace AppInstaller
             switch (argument.RunMode)
             {
                 case RunMode.CheckUpdate:
-                    return CheckUpdate(argument);
+                    return new CheckUpdate().Run(argument);
                 case RunMode.DownloadItemsToTemp:
                     return new DownloadItemsToTemp(appInstallerAssemblyName).Run(argument);
                 case RunMode.RunWithNewAppInTemp:
@@ -68,28 +66,5 @@ namespace AppInstaller
             }
         }
 
-        private static AppInstallerResult CheckUpdate(AppInstallerArgument appInstallerArgument)
-        {
-            var excludeRegexList = appInstallerArgument.ExcludePathRegex.Select(ex => new Regex(ex)).ToList();
-
-            var sourceDirInfo = new DirectoryInfo(appInstallerArgument.SourceDir);
-            var sourceLastUpdateDate = GetLastWriteTimeUtc(sourceDirInfo, excludeRegexList);
-
-            var installDirInfo = new DirectoryInfo(appInstallerArgument.InstallDir);
-            var installLastUpdateDate = GetLastWriteTimeUtc(installDirInfo, excludeRegexList);
-            return new AppInstallerResult
-            {
-                ResultCode = ResultCode.Success,
-                Updated = sourceLastUpdateDate > installLastUpdateDate
-            };
-
-        }
-
-        private static DateTime GetLastWriteTimeUtc(DirectoryInfo targetDirectoryInfo, IEnumerable<Regex> regex)
-        {
-            return targetDirectoryInfo.GetFiles("*", SearchOption.AllDirectories)
-                .Where(f => f.IsTargetFile(targetDirectoryInfo.FullName, regex))
-                .Max(f => f.LastWriteTimeUtc);
-        }
     }
 }
