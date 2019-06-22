9f3609fab68fe154cf52434370121b1acb377000
Kenjiro Nagao <toto1010@hotmail.com>
2019-06-22 14:18:30 +0900

    Adding wrapper

---
+++ DEV/Standard/EasySharpStandard/Wrappers/Implementation/DirectoryInfoWrapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using EasySharpStandard.Wrappers.Core;
+
+namespace EasySharpStandard.Wrappers.Implementation
+{
+    public class DirectoryInfoWrapper
+    {   }
+}
