4585b6ae1f8d83be6816cc01d5afdd14420f9511
Kenjiro Nagao <toto1010@hotmail.com>
2019-10-26 22:30:12 +0900

    adding     SizeRestrictedObservableCollection

---
+++ DEV/Standard/EasySharpStandardMvvm/Collections/SizeRestrictedObservableCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace EasySharpStandardMvvm.Collections
+{
+    public class SizeRestrictedObservableCollection<T> : ObservableCollection<T>
+    {
+        private readonly int max;
+
+        public SizeRestrictedObservableCollection(int max = 1000)
+        {
+            this.CollectionChanged += OnCollectionChanged;
+            this.max = max;
+        }
+
+        private void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add 
+                && this.max < this.Count)
+            {
+                for (int i = 0; i < this.Count - this.max; i++)
+                {
+                    this.RemoveAt(0);
+                }
+            }
+        }
+    }
+}
