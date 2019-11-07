7ae8bb56011e9fe1756777acfe08d5cb47a85f78
Kenjiro Nagao <toto1010@hotmail.com>
2019-11-07 21:53:18 +0900

    add struct extensions

---
+++ DEV/Standard/EasySharpStandard/Reflections/Core/StructExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace EasySharpStandard.Reflections.Core
+{
+    public static class StructExtensions
+    {
+        public static string ToFormattedString<T>(
+            this T instance,
+            string propertySeparator = "\t",
+            string valueSeparator = "=")
+            where T : struct
+        {
+            var type = instance.GetType();
+            var properties = type.GetProperties();
+            string[] values = new string[properties.Length];
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var property = properties[i];
+                values[i] = $"{property.Name}{valueSeparator}{property.GetValue(instance)}";
+            }
+
+            return string.Join(propertySeparator, values);
+        }
+    }
+}
