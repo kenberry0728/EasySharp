021decdd49a137cffb1a9d58d5ce44861c744b0f
Kenjiro Nagao <toto1010@hotmail.com>
2019-11-09 22:44:46 +0900

    EventContainer

---
+++ DEV/Standard/EasySharpStandard/EventContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EasySharp
+{
+    public class EventContainer<TEventArg>
+    {
+        private readonly Action<EventHandler<TEventArg>> subscribeEvent;
+        private readonly Action<EventHandler<TEventArg>> unsubscrieEvent;
+
+        public EventContainer(
+            Action<EventHandler<TEventArg>> subscribeEvent,
+            Action<EventHandler<TEventArg>> unsubscrieEvent)
+        {
+            this.subscribeEvent = subscribeEvent;
+            this.unsubscrieEvent = unsubscrieEvent;
+        }
+
+        public void SubscribeEvent(EventHandler<TEventArg> action)
+        {
+            this.subscribeEvent(action);
+        }
+
+        public void UnsubscribeEvent(EventHandler<TEventArg> action)
+        {
+            this.unsubscrieEvent(action);
+        }
+    }
+}
---
+++ DEV/Standard/EasySharpStandard/EventContainerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EasySharp
+{
+    public static class EventContainerExtensions
+    {
+        public static void DoOrReserve<TEventArg>(
+            this EventContainer<TEventArg> eventContainer,
+            Action action,
+            Func<bool> needToReserveAction)
+        {
+            eventContainer.SubscribeEvent(OnReservedEventTriggered);
+            if (!needToReserveAction())
+            {
+                UnsubscribeAndAction();
+            }
+
+            void OnReservedEventTriggered(object sender, TEventArg arg)
+            {
+                UnsubscribeAndAction();
+            }
+
+            void UnsubscribeAndAction()
+            {
+                eventContainer.UnsubscribeEvent(OnReservedEventTriggered);
+                action?.Invoke();
+            }
+        }
+    }
+}
