3ee83bf464b061338f81819e345baf6741b5c52d
Kenjiro Nagao <toto1010@hotmail.com>
2019-11-16 21:46:02 +0900

    StateOBserver-.ValueObserver

--- DEV/Standard/EasySharpStandard/Threading/Alarm.cs
+++ DEV/Standard/EasySharpStandard/Threading/Alarm.cs
@@ -6,7 +6,7 @@ using System.Threading;
 
 namespace EasySharp.Threading
 {
-    public class Alarm : StateObserverBase<AlarmState>
+    public class Alarm : ValueObserverBase<AlarmState>
     {
         private readonly Queue<DateTime> remainingSettingTimes;
         private readonly Timer timer;
