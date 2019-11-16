b1f8a770126b2c30c697f9534f1e0a6f18b3b415
Kenjiro Nagao <toto1010@hotmail.com>
2019-11-16 22:22:44 +0900

    ArgumentNullOrEmpty

--- DEV/Standard/EasySharpStandard/ObjectExtensions.cs
+++ DEV/Standard/EasySharpStandard/ObjectExtensions.cs
@@ -1,14 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EasySharp
 {
     public static class ObjectExtensions
     {
-        public static void ThrowArgumentNullException(this object argument, string argumentName)
+        public static void ThrowArgumentNullException<T>(this T argument, string argumentName)
+            where T : class
         {
             if (argument == null)
             {
                 throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        public static void ThrowArgumentNullOrEmptyException<T>(this IEnumerable<T> argument, string argumentName)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+
+            if (!argument.Any())
+            {
+                throw new ArgumentException(argumentName);
             }
         }
     }
