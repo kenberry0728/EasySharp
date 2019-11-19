9ec2bca6fe93ef081ba1e3903865aa0c705b216b
Kenjiro Nagao <toto1010@hotmail.com>
2019-11-20 06:16:09 +0900

    moduel namesspace

--- DEV/Standard/EasySharpStandard.Win/WindowHandle/EnumWindowsFromTitleService.cs
+++ DEV/Standard/EasySharpStandard.Win/WindowHandle/EnumWindowsFromTitleService.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace EasySharpStandard.Win.WindowHandle
+namespace EasySharp.Win.WindowHandle
 {
     public class EnumWindowsFromTitleService
     {
