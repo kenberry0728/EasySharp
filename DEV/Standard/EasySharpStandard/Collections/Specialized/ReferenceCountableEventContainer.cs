e5c6204358cf44c026a5c76fe03c22c80567c782
Kenjiro Nagao <toto1010@hotmail.com>
2019-12-10 22:47:49 +0900

    ReferenceCountableEventContainer

--- DEV/Standard/EasySharpStandard/Collections/Specialized/ReferenceCountableEventContainer.cs
+++ DEV/Standard/EasySharpStandard/Collections/Specialized/ReferenceCountableEventContainer.cs
@@ -1,20 +1,31 @@
-﻿using System;
+﻿using EasySharp.Collections.Specialized;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace EasySharp
 {
-    public class ReferenceCountableEventContainer<TEventArg> : EventContainer<TEventArg>, IReferenceCountableEventContainer<TEventArg>
+    public class ReferenceCountableEventContainer<TEventArg> 
+        : EventContainer<TEventArg>, IReferenceCountableEventContainer<TEventArg>
     {
-        private readonly List<EventHandler<TEventArg>> handlers = new List<EventHandler<TEventArg>>();
+        private readonly ObservableCollection<EventHandler<TEventArg>> handlers = new ObservableCollection<EventHandler<TEventArg>>();
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
 
         public ReferenceCountableEventContainer(
             Action<EventHandler<TEventArg>> subscribeEvent,
             Action<EventHandler<TEventArg>> unsubscribeEvent)
             :base(subscribeEvent, unsubscribeEvent)
         {
+            this.NotifyCollectionChanged = new NotifyCollectionChangedEventContainer(
+                handler => handlers.CollectionChanged += handler,
+                handler => handlers.CollectionChanged += handler);
         }
-
+        
         public int ReferenceCount => this.handlers.Count;
+
+        public INotifyCollectionChangedEventContainer NotifyCollectionChanged { get; }
 
         public override Action Subscribe(EventHandler<TEventArg> action)
         {
@@ -31,16 +42,23 @@ namespace EasySharp
 
     public class ReferenceCountableEventContainer : EventContainer, IReferenceCountableEventContainer
     {
-        private readonly List<EventHandler> handlers = new List<EventHandler>();
+        private readonly ObservableCollection<EventHandler> handlers = new ObservableCollection<EventHandler>();
 
         public ReferenceCountableEventContainer(
             Action<EventHandler> subscribeEvent,
             Action<EventHandler> unsubscribeEvent)
             : base(subscribeEvent, unsubscribeEvent)
         {
+            this.NotifyCollectionChanged = new NotifyCollectionChangedEventContainer(
+                handler => handlers.CollectionChanged += handler,
+                handler => handlers.CollectionChanged += handler);
         }
 
         public int ReferenceCount => this.handlers.Count;
+
+        public INotifyCollectionChangedEventContainer NotifyCollectionChanged { get; }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
 
         public override Action Subscribe(EventHandler action)
         {
