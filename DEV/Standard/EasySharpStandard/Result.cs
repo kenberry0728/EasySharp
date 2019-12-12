e106f371d3967a66a2b187e9bbc55d285351e6f3
Kenjiro Nagao <toto1010@hotmail.com>
2019-12-13 08:19:56 +0900

    Result

---
+++ DEV/Standard/EasySharpStandard/Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EasySharp
+{
+    public class Result<T>
+    {
+        public Result(bool ok, T value)
+        {
+            this.Ok = ok;
+            this.Value = value;
+        }
+
+        public bool Ok { get; }
+
+        public T Value { get; }
+    }
+
+    public class Ok<T> : Result<T> 
+    {
+        public Ok(T value)
+            : base(true, value)
+        {
+        }
+    }
+
+    public class Err<T> : Result<T>
+    {
+        public Err(T value, Exception exception)
+            : base(false, value)
+        {
+            this.Exception = exception;
+        }
+
+        public Exception Exception { get; }
+    }
+}
