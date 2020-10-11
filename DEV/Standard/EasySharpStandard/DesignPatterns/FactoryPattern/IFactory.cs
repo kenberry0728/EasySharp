55e11d57af8cbdef75a067dc92298f89f9b95745
Kenjiro Nagao <toto1010@hotmail.com>
2020-10-11 09:33:24 +0900

    fac

---
+++ DEV/Standard/EasySharpStandard/DesignPatterns/FactoryPattern/IFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace EasySharp.DesignPatterns.FactoryPattern
+{
+    public interface IFactory<TInstance>
+    {
+        TInstance Create();
+    }
+
+    public interface IFactory<TArgument, TInstance>
+    {
+        TInstance Create(TArgument arg);
+    }
+
+    public interface IFactory<TArgument1, TArgument2, TInstance>
+    {
+        TInstance Create(TArgument1 arg1, TArgument2 arg2);
+    }
+
+    public interface IFactory<TArgument1, TArgument2, TArgument3, TInstance>
+    {
+        TInstance Create(TArgument1 arg1, TArgument2 arg2, TArgument3 arg3);
+    }
+}
