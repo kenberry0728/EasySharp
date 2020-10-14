80bdc5b3d85505157de1620332ae2b18251cca82
Kenjiro Nagao <toto1010@hotmail.com>
2020-10-14 22:41:04 +0900

    cr

--- DEV/Standard/EasySharpStandard/Architecture/CRUD/ICrud.cs
+++ DEV/Standard/EasySharpStandard/Architecture/CRUD/ICrud.cs
@@ -1,8 +1,23 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace EasySharp.Architecture.CRUD
 {
-    public interface ICrud
+    public interface IKey<TKey>
     {
-        
+        TKey Key { get; }
+    }
+
+    public interface ICrud<TKey, TEntry>
+    where TEntry : IKey<TKey>
+    {
+        void Create(TEntry entry);
+
+        TEntry ReadOne(TKey identity);
+
+        IEnumerable<TEntry> ReadAll();
+
+        void Update(TEntry entry);
+
+        void Delete(TEntry entry);
     }
 }
