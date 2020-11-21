c191fd4b59689afa8fa4233bf99d17a4c577beca
長尾 健次郎 <toto1010@hotmail.com>
2020-11-21 20:57:46 +0900

    delete info interface

--- DEV/Standard/EasySharpStandard/Runtime/Serialization/IFileSystemInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.Serialization;
-
-namespace EasySharp.Runtime.Serialization
-{
-    public interface IFileSystemInfo : IO.IFileSystemInfo
-    {
-        void GetObjectData(SerializationInfo info, StreamingContext context);
-    }
-}
