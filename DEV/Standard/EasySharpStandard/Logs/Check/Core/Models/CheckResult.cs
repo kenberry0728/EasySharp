5296100fbdbdf303d840a51bdaf28617213331a5
長尾 健次郎 <toto1010@hotmail.com>
2020-11-22 09:41:33 +0900

    log

--- DEV/Standard/EasySharpStandard/Logs/Check/Core/Models/CheckResult.cs
+++ DEV/Standard/EasySharpStandard/Logs/Check/Core/Models/CheckResult.cs
@@ -1,16 +1,24 @@
-﻿namespace EasySharp.Logs.CheckLogs.Core.Models
+﻿using System.Collections.Generic;
+
+namespace EasySharp.Logs.CheckLogs.Core.Models
 {
     public class CheckResult<TErrorCode>
     {
-        public CheckResult(CheckResultCategory category, TErrorCode errorCode)
+        public CheckResult(
+            CheckResultCategory category,
+            TErrorCode errorCode,
+            params string[] detailInfo)
         {
             this.Category = category;
             this.ErrorCode = errorCode;
+            this.DetailInfo = detailInfo;
         }
 
         CheckResultCategory Category { get; }
 
         public TErrorCode ErrorCode { get; }
+        
+        public IEnumerable<string> DetailInfo { get; }
 
         public override string ToString()
         {
